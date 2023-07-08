56352315b15360a7b1c1b57a1377651125ba80cb
Mauricio M <mcm.crw@gmail.com>
2023-07-08 10:09:28 -0400

    feat: example

--- web-scraping/los-tiempos/main.py
+++ web-scraping/los-tiempos/main.py
@@ -1,40 +1,79 @@
 from utils import get, join_urls
 from bs4 import BeautifulSoup
+import logging
 
 URL = "https://www.lostiempos.com/"
 
-# Obtener el HTML puro como string
-content = get(URL)
-# Pasar el string HTML  a BeautifulSoup (bs4 lo interpreta y ahora se 
-# puede usar como un objeto para realizar todo tipo de consultas)
-soup = BeautifulSoup(content, "html.parser")
-
-# Titulo del sitio
-title = soup.title.get_text()
-
-print(title)
-
-# Listar todas las urls del sitio
-links = soup.find_all("a")
-for link in links:
-  url = link["href"]
-  if  not ("http" in url):
-    print(join_urls(URL, url))
-  else: 
-    print(url)
+# # Configurando un logger
+# logging.basicConfig(filename="ejemplo.log", encoding="utf-8", level=logging.INFO)
 
 
-# Ver los títulos de las noticias en el sitio
-news = soup.find_all("div", { 
-  "class": "noticia-paywall1"
-})
-news = soup.select(".noticia-paywall1")
-print(len(news))
+# # Obtener el HTML puro como string
+# content = get(URL)
+# # Pasar el string HTML  a BeautifulSoup (bs4 lo interpreta y ahora se
+# # puede usar como un objeto para realizar todo tipo de consultas)
+# soup = BeautifulSoup(content, "html.parser")
 
-for item in news:
-  title = item.select(".views-field-title")
-  title = title[0] if len(title) else None
-  if title:
-    print(title.get_text())
+# # Obtener título del sitio
+# title = soup.title.get_text()
+# print(title)
+
+# # Listar todas las urls del sitio
+# links = soup.find_all("a")
+# for link in links:
+#     url = link["href"]
+#     if "http" in url:
+#         print(url)
+#     else:
+#         print(join_urls(URL, url))
 
 
+# # Ver los títulos de las noticias en el sitio
+# news = soup.find_all("div", {"class": "noticia-paywall1"})
+# news = soup.select(".noticia-paywall1")
+# for item in news:
+#     title = item.select(".views-field-title")
+#     title = title[0] if len(title) else None
+#     if title:
+#         print(title.get_text().strip())
+
+
+class LosTiemposPage:
+    def __init__(self, url) -> None:
+        self.url = url
+        content = get(URL)
+        self.soup = BeautifulSoup(content, "html.parser")
+
+    def get_urls(self):
+        urls = []
+        links = self.soup.find_all("a")
+        for link in links:
+            url = link["href"]
+            if "http" in url:
+                urls.append(url)
+            else:
+                urls.append(join_urls(URL, url))
+        return urls
+
+    def get_title(self):
+        return self.soup.title.get_text()
+
+    def get_body(self):
+        return
+
+    def get_images(self):
+        urls = []
+        images = self.soup.find_all("img")
+        for image in images:
+            url = image["src"]
+            if "http" in url:
+                urls.append(url)
+            else:
+                urls.append(join_urls(self.url, url))
+        return urls
+
+losTiempos = LosTiemposPage(url=URL)
+
+print(losTiempos.get_title())
+
+# logging.info('Mensaje de prueba')
--- web-scraping/los-tiempos/utils.py
+++ web-scraping/los-tiempos/utils.py
@@ -1,5 +1,7 @@
 import requests
 from urllib import parse
+import os 
+import wget
 
 def get(url):
   response = requests.get(url, headers={
@@ -8,6 +10,11 @@ def get(url):
   content = response.content
   return content
 
-
 def join_urls(base, url):
   return parse.urljoin(base, url)
+
+def download_file(url, name="", base_dir="./data"):
+    dir_name = os.path.join(base_dir, name)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    return wget.download(url, out=dir_name)
